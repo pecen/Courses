47d9c2fdcd3dc88feba50a44d6a19e9a571d09b9
Peter Centellini <peter.c@trivec.se>
2019-06-02 21:09:12 +0200

    Initial Commit with starting point code and pages from chapter 2

---
+++ Mastering Xamarin Forms Development/Paperboy/Paperboy/Paperboy/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Paperboy {
+  public partial class MainPage : TabbedPage {
+    public MainPage() {
+      InitializeComponent();
+    }
+
+    private async void OnSettingsClicked(object sender, EventArgs e) {
+      await Navigation.PushAsync(new Pages.SettingsPage());
+    }
+  }
+}
